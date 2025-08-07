dc2c30812093351127fdfc657c983b98a06571c2
i582 <51853996+i582@users.noreply.github.com>
2025-08-07 23:08:15 +0400

    Updates

--- assets/liquid-CYG6zUoh.js
+++ assets/liquid-CYG6zUoh.js
@@ -1,4 +1,4 @@
-import{m as l}from"./monaco-editor-core-CpJEhaGI.js";/*!-----------------------------------------------------------------------------
+import{m as l}from"./monaco-editor-core-DLgf_DVT.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
