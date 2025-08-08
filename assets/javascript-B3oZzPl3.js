7fa18da251c65c4aa5185825dd085aaa2ad5737f
i582 <51853996+i582@users.noreply.github.com>
2025-08-08 14:12:00 +0400

    Updates

--- assets/javascript-B3oZzPl3.js
+++ assets/javascript-B3oZzPl3.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-Ch-nhXKA.js";import"./monaco-editor-core-BNZEfIW9.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-QyR3meem.js";import"./monaco-editor-core-DLgf_DVT.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
